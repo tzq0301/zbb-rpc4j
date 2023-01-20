5455939187900b833921d70d845bf349bc85bd8e
tzq0301 <1661610563@qq.com>
2023-01-20 19:55:45 +0800

    LogMiddleware

--- src/main/java/org/example/server/middleware/impl/LogMiddleware.java
+++ src/main/java/org/example/server/middleware/impl/LogMiddleware.java
@@ -1,19 +1,17 @@
 package org.example.server.middleware.impl;
 
+import lombok.extern.java.Log;
 import org.example.server.middleware.RpcMiddleware;
 import org.example.server.service.RpcService;
 
+@Log
 public class LogMiddleware implements RpcMiddleware {
     @Override
     public RpcService apply(RpcService service) {
         return (req, resp) -> {
-            System.out.println("Log: " + req);
-            System.out.flush();
-
+            log.info(String.format("Request:  %s", req.toString()));
             service.accept(req, resp);
-
-            System.out.println("Log: " + resp);
-            System.out.flush();
+            log.info(String.format("Response: %s", resp.toString()));
         };
     }
 }
